9982a79c7d2128fdd6c847bd19ee6585a0ce9ec5
liborgabrhel <gabrhel.libor@gmail.com>
2019-12-20 14:14:38 +0000

    #5: Added theme and site fonts

---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '#8C2035',
+  secondary: '#574142',
+  tertiary: '#BFA5A6',
+}
+
+export const theme = {
+  colors: colors,
+  font: {
+    family: {
+      primary: `"Roboto", sans-serif`,
+      secondary: `"IBM Plex Sans", sans-serif`,
+    }
+  }
+}
