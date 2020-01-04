a0144710a9473045709ab28d86f6f04b37249896
liborgabrhel <gabrhel.libor@gmail.com>
2020-01-04 18:37:46 +0000

    Added header a scroll effect

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,48 +1,65 @@
 import { Logo } from "components/logo"
 import { Link } from "gatsby"
-import React from "react"
+import React, { useLayoutEffect, useState } from "react"
 import { routes } from "routes"
 import styled from "styled-components"
 import { theme } from "theme"
 import { Navigation } from "../navigation"
 import { NavigationLink } from "../navigation-link"
 
+export const HEADER_HEIGHT = 80
+
 type Props = {
   children?: never
 }
 
-export const Header = (_: Props) => (
-  <Container>
-    <Wrapper>
-      <Link to={"/"}>
-        <Logo height={"20px"} width={"auto"} />
-      </Link>
-      <Navigation>
-        <NavigationLink to={routes.cs.home}>{`Úvod`}</NavigationLink>
-        <NavigationLink to={routes.cs.services}>{`Služby`}</NavigationLink>
-        <NavigationLink to={routes.cs.pricing}>{`Ceny`}</NavigationLink>
-        <NavigationLink to={routes.cs.about}>{`O nás`}</NavigationLink>
-        <NavigationLink to={routes.cs.contact}>{`Kontakt`}</NavigationLink>
-      </Navigation>
-    </Wrapper>
-  </Container>
-)
+export const Header = (_: Props) => {
+  const [isTransparent, setIsTransparent] = useState(true)
+
+  useLayoutEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > HEADER_HEIGHT / 2) {
+        setIsTransparent(false)
+      } else {
+        setIsTransparent(true)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
+  return (
+    <Container isTransparent={isTransparent}>
+      <Wrapper>
+        <Link to={"/"}>
+          <Logo height={"20px"} width={"auto"} />
+        </Link>
+        <Navigation>
+          <NavigationLink to={routes.cs.home}>{`Úvod`}</NavigationLink>
+          <NavigationLink to={routes.cs.services}>{`Služby`}</NavigationLink>
+          <NavigationLink to={routes.cs.pricing}>{`Ceny`}</NavigationLink>
+          <NavigationLink to={routes.cs.about}>{`O nás`}</NavigationLink>
+          <NavigationLink to={routes.cs.contact}>{`Kontakt`}</NavigationLink>
+        </Navigation>
+      </Wrapper>
+    </Container>
+  )
+}
 
 /**
  * Styled components
  */
 
-export const HEADER_HEIGHT = 80
-
-const Container = styled("header")`
+const Container = styled("header")<{ isTransparent: boolean }>`
   display: flex;
   position: fixed;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: ${HEADER_HEIGHT}px;
-  background-color: ${theme.header.backgroundColor};
-  box-shadow: 0 2px 4px ${theme.colors.shadow};
+  background-color: ${props => (props.isTransparent ? "transparent" : theme.header.backgroundColor)};
+  box-shadow: ${props => (props.isTransparent ? "none" : `0 2px 4px ${theme.colors.shadow}`)};
+  transition: all 250ms ease-in-out;
 `
 
 const Wrapper = styled("div")`
@@ -54,5 +71,5 @@ const Wrapper = styled("div")`
   width: 100%;
   height: 100%;
   max-width: 1024px;
-  padding: 0 20px;
+  padding: 0 40px;
 `
